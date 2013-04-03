fb83a5878469616a244196c059600f8a0f53029a
Daniele Bellavista <daniele.bellavista@gmail.com>
2013-04-03 10:04:01 +0200

    Configuration files skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cetf::Application.config.secret_token = '3797058528b8c69470ee9c733c4a57ed3f60049ad5d25a04e64969c0d0915f4aa22b5015bb426d9afeb4d3aeac40e27ee48b35ab9250bfcc3e6daade111814a9'
